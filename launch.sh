d1b91b279eef7e74356929c630a8e1ffdefc0d00
Sanne Grinovero <sanne@hibernate.org>
2018-10-23 11:13:56 +0100

    Fix the launcher script

--- launch.sh
+++ launch.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 echo "Starting mvn build..."
-mvn clean package > maven-build.log
+mvn clean package
 
 ## Update the classpath definition used by this script:
-mvn dependency:build-classpath -DexcludeArtifactIds=graal-annotations -Dmdep.outputFile=cp.txt >> maven-build.log
+mvn dependency:build-classpath -DexcludeArtifactIds=graal-annotations -Dmdep.outputFile=cp.txt
 CLASSPATH=`cat cp.txt`
 
 echo "CLASSPATH: $CLASSPATH"
 PROFILER_OPT=""
 
-TUNING_OPT="-XX:MetaspaceSize=80M -Xmx1G -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+PrintStringDeduplicationStatistics"
+#-XX:+UseStringDeduplication -XX:+PrintStringDeduplicationStatistics
+TUNING_OPT="-XX:MetaspaceSize=80M -Xmx1G -XX:+UseG1GC "
 #PROFILER_OPT="-agentpath:/opt/jprofiler10/bin/linux-x64/libjprofilerti.so=offline,id=128,config=/home/sanne/.jprofiler10/config.xml"
 
 java $TUNING_OPT $PROFILER_OPT -cp "$CLASSPATH":./target/classes org.hibernate.orm.jmh.StandardLaunch
+
